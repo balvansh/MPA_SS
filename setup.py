da587dc950b3868c1e062ab644b569003ace0f60
ThorodanBrom <bpr_95@hotmail.com>
2018-03-21 00:20:21 +0530

    setup.py - share generation

---
+++ setup.py
@@ -0,0 +1,5 @@
+# create db if not existing
+# get threshold
+# autoset the number of shares and send mail
+# distribute shares to users when they are connected
+# see if the VPN can assign static IP addresses -> makes job easier
